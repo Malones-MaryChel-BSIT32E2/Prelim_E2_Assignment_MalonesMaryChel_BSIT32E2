009fcd50b1e93f0331c3b2269f6bb555f2b5c51e
STUDENT <STUDENT@DESKTOP-5L8Q2MB>
2024-03-02 13:01:18 +0800

    Add project files.

---
+++ Prelim_E2_Assignment_MalonesMaryChel_BSIT32E2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prelim_E2_Assignment_MalonesMaryChel_BSIT32E2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
